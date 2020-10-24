1be2f8079d664b752d4c63cd7f5828e2f33326cb
jaymzee <jaymzee@users.noreply.github.com>
2020-10-24 01:16:17 -0400

    FlangerFilter

---
+++ FlangerFilter.h
@@ -0,0 +1,12 @@
+#include "IFilter.h"
+#include "FractionalDelay.h"
+
+class Flanger: public IFilter {
+public:
+    FractionalDelay w;  // delay line
+    double rate;        // rate of flanger
+    double phase;       // current phase of flanger (time)
+
+    Flanger(unsigned len, double rate) : w(len), rate(rate), phase(0.0) {}
+    float ProcessSample(float x); // process one sample
+};
--- wavFlanger.cpp
+++ wavFlanger.cpp
@@ -1,35 +1,7 @@
-#include "IFilter.h"
-#include "FractionalDelay.h"
+#include "FlangerFilter.h"
 #include "Wave.hpp"
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
-
-#define PI 3.14159265358979323846
-#define FS 44100
-
-class Flanger: public IFilter {
-public:
-    FractionalDelay w;  // delay line
-    double rate;        // rate of flanger
-    double phase;       // current phase of flanger (time)
-
-    Flanger(unsigned len, double rate) : w(len), rate(rate), phase(0.0) {}
-
-    // process one sample
-    float ProcessSample(float x) {
-        const int N = w.Length();
-        double n, y;
-
-        w[0] = x;
-        n = (N - 1) * (0.5 * cos(2 * PI * rate * phase) + 0.5);
-        y = 0.5 * w[N / 2] + 0.5 * w[n];
-        phase += 1.0 / FS;
-        --w;
-
-        return y;
-    }
-};
 
 int main(int argc, char *argv[])
 {
@@ -37,9 +9,8 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: wavFlanger infile outfile\n");
         return EXIT_FAILURE;
     }
-    const char *infile = argv[1];
-    const char *outfile = argv[2];
+
     Flanger f(200, 0.125);
 
-    return WaveFilter(infile, outfile, &f, WAVE_PCM, 0.0);
+    return WaveFilter(argv[1], argv[2], &f, WAVE_PCM, 0.0);
 }
