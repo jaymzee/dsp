f80b1a1583e0f569faa1759408ef5a9e5df505e3
jaymzee <jaymzee@users.noreply.github.com>
2020-11-20 09:28:40 -0500

    clang for windows makefile updated

--- clang-win.mk
+++ clang-win.mk
@@ -1,65 +1,64 @@
-CC = clang
-CXX = clang++
-CFLAGS = -O2 -std=c89 -pedantic
-CXXFLAGS = -O2 -std=c++14 -pedantic
+CC = gcc
+CXX = g++
+CFLAGS = -O2 -std=c89 -pedantic -Wall
+CXXFLAGS = -g -O2 -std=c++11 -pedantic -Wall
 LFLAGS =
-OBJSBASIC = wavdump.exe wav_write.exe
-OBJSFLTRC = wav_can_filt.exe wav_reverb.exe wav_flanger.exe
-OBJSFLTRCPP = wavCanFilt.exe wavReverb.exe wavFlanger.exe
+OBJSBASIC = wavdump.exe wavwrite.exe
+OBJSFLTRC = wavcanfltr.exe wav_reverb.exe wav_flanger.exe
+OBJSFLTRCPP = wavdir1.exe wavdir2.exe wavdir2t.exe wavbiquad.exe wavReverb.exe wavFlanger.exe
 OBJS = $(OBJSBASIC) $(OBJSFLTRC) $(OBJSFLTRCPP)
 
 all: $(OBJS)
 
-wavCanFilt.exe: wavCanFilt.o wave.o CanonicalFilter.o
-	$(CXX) -o $@ $^ $(LFLAGS)
-wavReverb.exe: wavReverb.o wave.o CircularFilter.o
-	$(CXX) -o $@ $^ $(LFLAGS)
-wavFlanger.exe: wavFlanger.o wave.o FlangerFilter.o FractionalDelay.o
-	$(CXX) -o $@ $^ $(LFLAGS)
+wavdir1.exe: wavdir1.cpp wave.o directform.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LFLAGS)
+wavdir2.exe: wavdir2.cpp wave.o directform.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LFLAGS)
+wavdir2t.exe: wavdir2t.cpp wave.o directform.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LFLAGS)
+wavbiquad.exe: wavbiquad.cpp wave.o biquad.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LFLAGS)
+wavReverb.exe: wavReverb.cpp wave.o circular.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LFLAGS)
+wavFlanger.exe: wavFlanger.cpp wave.o delay.o
+	$(CXX) $(CXXFLAGS) -o $@ $^ -lm $(LFLAGS)
 
-wavCanFilt.o: wavCanFilt.cpp Wave.hpp CanonicalFilter.h
+directform.o: directform.cpp directform.h
 	$(CXX) $(CXXFLAGS) -c $<
-wavReverb.o: wavReverb.cpp Wave.hpp CircularFilter.h
+biquad.o: biquad.cpp biquad.h
 	$(CXX) $(CXXFLAGS) -c $<
-wavFlanger.o: wavFlanger.cpp Wave.hpp FlangerFilter.h
+circular.o: circular.cpp circular.h
 	$(CXX) $(CXXFLAGS) -c $<
-
-CanonicalFilter.o: CanonicalFilter.cpp CanonicalFilter.h
-	$(CXX) $(CXXFLAGS) -c $<
-CircularFilter.o: CircularFilter.cpp CircularFilter.h
-	$(CXX) $(CXXFLAGS) -c $<
-FractionalDelay.o: FractionalDelay.cpp FractionalDelay.h
-	$(CXX) $(CXXFLAGS) -c $<
-FlangerFilter.o: FlangerFilter.cpp FlangerFilter.h FractionalDelay.h
+delay.o: delay.cpp delay.h
 	$(CXX) $(CXXFLAGS) -c $<
 
-wav_flanger.exe: wav_flanger.o wave.o fractional_delay.o
+wavcanfltr.exe: wavcanfltr.o wave.o canfltr.o
 	$(CC) -o $@ $^ $(LFLAGS)
-wav_can_filt.exe: wav_can_filt.o wave.o canonical_filter.o
-	$(CC) -o $@ $^ $(LFLAGS)
-wav_reverb.exe: wav_reverb.o wave.o circular_filter.o
+wav_flanger.exe: wav_flanger.o wave.o delayline.o
+	$(CC) -o $@ $^ -lm $(LFLAGS)
+wav_reverb.exe: wav_reverb.o wave.o cirfltr.o
 	$(CC) -o $@ $^ $(LFLAGS)
 
-wav_flanger.o: wav_flanger.c wave.h fractional_delay.h
+wavcanfltr.o: wavcanfltr.c wave.h canfltr.h
 	$(CC) $(CFLAGS) -c $<
-wav_can_filt.o: wav_can_filt.c wave.h canonical_filter.h
+wav_flanger.o: wav_flanger.c wave.h delayline.h
 	$(CC) $(CFLAGS) -c $<
-wav_reverb.o: wav_reverb.c wave.h circular_filter.h
-	$(CC) $(CFLAGS) -c $<
-
-fractional_delay.o: fractional_delay.c fractional_delay.h
-	$(CC) $(CFLAGS) -c $<
-canonical_filter.o: canonical_filter.c canonical_filter.h
-	$(CC) $(CFLAGS) -c $<
-circular_filter.o: circular_filter.c circular_filter.h
+wav_reverb.o: wav_reverb.c wave.h cirfltr.h
 	$(CC) $(CFLAGS) -c $<
 
-wav_write.exe: wav_write.o wave.o
-	$(CC) -o $@ $^ $(LFLAGS)
+delayline.o: delayline.c delayline.h
+	$(CC) $(CFLAGS) -c $<
+canfltr.o: canfltr.c canfltr.h
+	$(CC) $(CFLAGS) -c $<
+cirfltr.o: cirfltr.c cirfltr.h
+	$(CC) $(CFLAGS) -c $<
+
+wavwrite.exe: wavwrite.o wave.o
+	$(CC) -o $@ $^ -lm $(LFLAGS)
 wavdump.exe: wavdump.o wave.o
 	$(CC) -o $@ $^ $(LFLAGS)
 
-wav_write.o: wav_write.c wave.h
+wavwrite.o: wavwrite.c wave.h
 	$(CC) $(CFLAGS) -c $<
 wavdump.o: wavdump.c wave.h
 	$(CC) $(CFLAGS) -c $<
