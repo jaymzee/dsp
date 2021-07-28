e9a82111e3532efda34cc3d4841039efde6d41d5
jaymzee <jaymzee@users.noreply.github.com>
2021-07-28 14:41:40 -0400

    convolution reorder parameters

--- convolve.c
+++ convolve.c
@@ -2,41 +2,51 @@
 
 #define MAX(a, b) ((a) > (b) ? a : b)
 #define MIN(a, b) ((a) < (b) ? a : b)
-#define NUM_OF_ELEM(x) (sizeof(x)/sizeof(*x))
 
-/* x is length L input array
- * h is length M input array
- * y is length L + M - 1 output array */
-void convolve(float y[], float x[], float h[], int L, int M)
+/* x: input samples of length L
+ * h: filter impulse response of length M
+ * y: output samples of length L + M - 1 */
+float *convolve(const float *x, int L, const float *h, int M, float *y)
 {
-    int i, k, j;
+    int i, j, k;
+    double acc;
+
     for (i = 0; i < L + M - 1; i++) {
         k = MIN(i, M - 1);
         j = MAX(i - M + 1, 0);
-        y[i] = 0.0;
-        while (j < MIN(i + 1, L)) {
-            y[i] += h[k--] * x[j++];
+        for (acc = 0.0; j < MIN(i + 1, L); j++, k--) {
+            acc += h[k] * x[j];
         }
+        y[i] = acc;
     }
+
+    return y;
 }
 
-void printarray(float x[], int len)
+void printarray(const char *label, const float *x, int len)
 {
+    int i;
     printf("[");
-    for (int i = 0; i < len; i++) {
+    for (i = 0; i < len; i++) {
         printf("%s%.3f", i > 0 ? " ":"", x[i]);
     }
     printf("]\n");
 }
 
-int main()
+void it_commutes(void)
 {
-    float a[] = {1.0, 2.0, 3.0};
-    float b[] = {4.0, 5.0};
+    float x[] = {1.0, 2.0, 3.0};
+    float h[] = {4.0, 5.0};
     float y[4];
 
-    convolve(y, a, b, 3, 2);
-    printarray(y, NUM_OF_ELEM(y));
-    convolve(y, b, a, 2, 3);
-    printarray(y, NUM_OF_ELEM(y));
+    printf("the convolution operator is commutative:\n");
+    printarray("x * h", convolve(x, 3, h, 2, y), 4);
+    printarray("h * x", convolve(h, 2, x, 3, y), 4);
+}
+
+int main()
+{
+    it_commutes();
+
+    return 0;
 }
